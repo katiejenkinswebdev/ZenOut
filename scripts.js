9117ce7da8e94ceddfeac7c0bcb3bb861b07c52d
KatieAJenkins <kind2karma@gmail.com>
2016-10-29 17:34:04 -0600

    inital commit with 2 osc working

---
+++ scripts.js
@@ -0,0 +1,57 @@
+'use strict';
+//allow to work on multiple browsers
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+//create destination
+const context = new AudioContext();
+
+//create Master Volume gain node and connect to destination
+
+//KT research - global effects compressor
+var masterVolume = context.createGain();
+masterVolume.gain.value = 1;
+masterVolume.connect(context.destination);
+// masterVolume.volume.value = 1;
+
+//create oscillatorLeft (sound)
+var oscillatorLeft = context.createOscillator();
+
+//create gainLeft (volume) node
+var gainLeft = context.createGain();
+
+//connect oscillator to gain node
+oscillatorLeft.connect(gainLeft);
+
+//connect gain to destination
+gainLeft.connect(masterVolume);
+
+//set oscillator starting frequency
+oscillatorLeft.frequency.value = 100;
+
+//set type of sound wave for oscillator
+oscillatorLeft.type = 'sine';
+
+//start oscillator
+oscillatorLeft.start();
+
+// Left volume control
+gainLeft.volume.value = 1;
+// gainLeft.volume.value = 0;
+
+//create oscillatorRight and gainRight
+var oscillatorRight = context.createOscillator();
+var gainRight = context.createGain();
+
+oscillatorRight.connect(gainRight);
+gainRight.connect(masterVolume);
+
+oscillatorRight.frequency.value = 400;
+oscillatorRight.type = 'sine';
+oscillatorRight.start();
+gainRight.volume.value = 1;
+// gainRight.volume.value = 0;
+
+
+
+//create master gain node = name it specifically var masterGainNode
+//create another oscillator = name that specifically oscLeft/oscRight = give each their own gain...gainLeft/gainRight. gain and osc will always be paired together. set up pan node (look up...prolly between 0 and 100) and connect between osc and gain...so osc pan gain
